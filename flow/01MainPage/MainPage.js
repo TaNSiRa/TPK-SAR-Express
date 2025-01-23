fac3b0f91df1e6b084667d9fd29b311b4fee29b2
TaNSiRa <tansirawit2543@gmail.com>
2025-01-23 16:42:11 +0700

    .

--- flow/01MainPage/MainPage.js
+++ flow/01MainPage/MainPage.js
@@ -40,4 +40,42 @@ router.post(
   }
 );
 
+router.post(
+  "/MainPage_DeleteStageReason",
+  async (req, res) => {
+    console.log("in DeleteStageReason");
+    try {
+      var dataIn = JSON.parse(req.body.DeleteKPIreason);
+      var reqNo = dataIn[0].ReqNo;
+      var reason = '';
+      var stage = '';
+
+      var query = `
+        UPDATE [KPI_Overdue]
+        SET 
+          Reason1 = CASE WHEN ReqNo1 = '${reqNo}' THEN '${reason}' ELSE Reason1 END,
+          Reason2 = CASE WHEN ReqNo2 = '${reqNo}' THEN '${reason}' ELSE Reason2 END,
+          Reason3 = CASE WHEN ReqNo3 = '${reqNo}' THEN '${reason}' ELSE Reason3 END,
+          Reason4 = CASE WHEN ReqNo4 = '${reqNo}' THEN '${reason}' ELSE Reason4 END,
+          Stage1 = CASE WHEN ReqNo1 = '${reqNo}' THEN '${stage}' ELSE Stage1 END,
+          Stage2 = CASE WHEN ReqNo2 = '${reqNo}' THEN '${stage}' ELSE Stage2 END,
+          Stage3 = CASE WHEN ReqNo3 = '${reqNo}' THEN '${stage}' ELSE Stage3 END,
+          Stage4 = CASE WHEN ReqNo4 = '${reqNo}' THEN '${stage}' ELSE Stage4 END
+        WHERE '${reqNo}' IN (ReqNo1, ReqNo2, ReqNo3, ReqNo4);
+      `;
+
+      console.log(query);
+      await mssqlKPI.qurey(query);
+
+
+
+      res.send("OK");
+    } catch (error) {
+      console.log(error);
+      res.status(400);
+      res.json("ERROR");
+    }
+  }
+);
+
 module.exports = router;
