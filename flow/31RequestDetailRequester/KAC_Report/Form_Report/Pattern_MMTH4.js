a3d16f4383aac76e71b417b010ebcaddaa48eb6c
TaNSiRa <tansirawit2543@gmail.com>
2025-05-27 08:49:41 +0700

    .

--- flow/31RequestDetailRequester/KAC_Report/Form_Report/Pattern_MMTH4.js
+++ flow/31RequestDetailRequester/KAC_Report/Form_Report/Pattern_MMTH4.js
@@ -772,7 +772,8 @@ async function PicSet(dataReport, doc, currentY) {
     ]);
     dataInTable.push([
       dataReport[startIndex + 12].ItemReportName,
-      dataReport[startIndex + 12].ControlRange,
+      // dataReport[startIndex + 12].ControlRange,
+      "-",
       dataReport[startIndex + 12].ResultReport,
       dataReport[startIndex + 13].ResultReport,
       dataReport[startIndex + 14].ResultReport,
--- function/mssql.js
+++ function/mssql.js
@@ -1,9 +1,9 @@
 const mssql = require('mssql');
 const config = {
   user: "sa",
-  password: "12345678",
+  password: "Automatic",
   database: "SAR",
-  server: "127.0.0.1",
+  server: "172.23.10.51",
   pool: {
     //max: 10,
     //min: 0,
